29a70be1891e9dd5c7845ae319390b0f7cdc72e7
DAMARLA GAGAN SRI RAM <vtu29458@veltech.edu.in>
2025-10-19 23:46:13 +0530

    Delete task 3 directory

--- task 3/4b.py
+++
@@ -1,8 +0,0 @@
-list=[]
-for i in range(int(input())):
-    name=input()
-    score=float(input())
-    list.append([name,score])
-second_highest=sorted(set([score for name,score in list]))[1]
-print('\n'.join(sorted([name for name,score in list if score==second_highest])))
-               
